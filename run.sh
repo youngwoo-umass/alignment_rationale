6af15ea884f78661f089d9b76d6e6d565dbb21bd
clover3 <lesterny@gmail.com>
2022-11-12 17:42:31 -0500

    Add data files

---
+++ run.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=src
+python src/main_code/executable/run_predict.py
+python src/main_code/executable/run_eval_for_all_metrics.py
+python src/main_code/executable/show_preference.py
+
+
+sleep 10
